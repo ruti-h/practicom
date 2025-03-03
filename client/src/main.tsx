25742019aca2d9dcf8ba78e991ba5bd31f3125e6
ruti-h <r0548428867@gmail.com>
2025-03-03 19:01:29 +0200

    practicom

---
+++ client/src/main.tsx
@@ -0,0 +1,16 @@
+// import { createApp } from 'vue'
+// import './index.css'
+// import App from './App.tsx'
+
+// createApp(App).mount('#app')
+
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+
+// createRoot(document.getElementById('root')!).render(
+// //   <StrictMode>
+// //     <App />
+// //   </StrictMode>,
+// )
